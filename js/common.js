1fc1b5f986838a887e06fe0f8742de09116f86f4
TtunMill <84768554+ttunmill@users.noreply.github.com>
2024-06-19 18:12:49 +0900

    main page

--- js/common.js
+++ js/common.js
@@ -26,3 +26,33 @@ const swiper2 = new Swiper(".sec02_swiper", {
         prevEl:".swiper-button-prev"
     },
 })
+
+const swiper3 = new Swiper(".sec04_swiper", {
+    autoplay : false,
+    loop : true,
+    slidesPerView: 2,
+    pagination: {
+        el: '.sec04_swiper .swiper-pagination',
+        clickable: true,
+        // type: 'fraction',
+    },
+    navigation: {
+        // nextEl:".swiper-button-next", 
+        // prevEl:".swiper-button-prev"
+    },
+})
+
+const swiper4 = new Swiper(".sec05_swiper", {
+    autoplay : false,
+    loop : true,
+    slidesPerView: 1,
+    pagination: {
+        el: '.sec04_swiper .swiper-pagination',
+        clickable: true,
+        // type: 'fraction',
+    },
+    navigation: {
+        // nextEl:".swiper-button-next", 
+        // prevEl:".swiper-button-prev"
+    },
+})
--- js/index.js
+++ js/index.js
@@ -6,10 +6,6 @@ lang_btn.style.display = "none"
 
 // sec03 이벤트 버튼
 const sec03_btn = document.querySelectorAll(".event_btn a")
-const all = document.querySelector(".all_content");
-const special = document.querySelector(".special_content");
-const fan = document.querySelector(".fan_content");
-const lucky = document.querySelector(".lucky_content");
 const sec03_ul = document.querySelectorAll(".sec03 > .all_list")
 
 sec03_btn[0].style.color = "#0a5e5a"
@@ -36,5 +32,4 @@ let text = ""
 img_g.forEach((i, j) => {
     text = sec03_tit[j].innerText
     hover_txt[j].innerText = text
-    console.log(hover_txt[j])
 })
