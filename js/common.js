913ec52d31610483481db329463cbd9f7fb9dd4b
TtunMill <84768554+ttunmill@users.noreply.github.com>
2024-06-20 18:11:18 +0900

    modal

--- js/common.js
+++ js/common.js
@@ -27,20 +27,42 @@ const swiper2 = new Swiper(".sec02_swiper", {
     },
 })
 
+// const swiper3 = new Swiper(".sec04_swiper", {
+//     autoplay : false,
+//     slidesPerView: 3,
+//     slidesPerGroup: 3,
+//     centeredSlides: true,
+//     initialSlide: 1,
+//     pagination: {
+//         el: '.sec04_swiper .swiper-pagination',
+//         clickable: true,
+//         type: 'bullets',
+//         type: 'fraction',
+//     },
+//     navigation: {
+//         nextEl:".swiper-button-next", 
+//         prevEl:".swiper-button-prev"
+//     },
+// })
+
 const swiper3 = new Swiper(".sec04_swiper", {
-    autoplay : false,
-    loop : true,
-    slidesPerView: 2,
+    autoplay: false,
+    slidesPerView: 3, // 한 번에 보여지는 슬라이드 개수
+    centeredSlides: true,
+    initialSlide: 1, // 초기 시작 슬라이드
     pagination: {
         el: '.sec04_swiper .swiper-pagination',
-        // clickable: true,
-        // type: 'fraction',
+        type: 'bullets', 
+        bulletClass: 'sec04-bullet', // 사용자 정의 클래스 지정
+        bulletActiveClass: 'sec04-bullet-active', // 활성화된 bullet의 사용자 정의 클래스 지정
     },
     navigation: {
-        // nextEl:".swiper-button-next", 
-        // prevEl:".swiper-button-prev"
+        nextEl:".swiper-button-next", 
+        prevEl:".swiper-button-prev"
     },
 })
+
+
 
 const swiper4 = new Swiper(".sec05_swiper", {
     autoplay : false,
