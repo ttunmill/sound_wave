db757b7c354d5b7edc7aecf154b0d8df8f112d69
TtunMill <84768554+ttunmill@users.noreply.github.com>
2024-06-24 16:56:50 +0900

    Update login.js

--- pages/js/login.js
+++ pages/js/login.js
@@ -5,11 +5,15 @@ const non_login_frm = document.querySelector("#non_user_form")
 non_login_frm.style.display = "none"
 
 member_btn[0].addEventListener("click", () => {
+    member_btn[1].classList.remove("active")
     non_login_frm.style.display = "none"
+    member_btn[0].classList.add("active")
     login_frm.style.display = "block"
 })
 
 member_btn[1].addEventListener("click", () => {
+    member_btn[0].classList.remove("active")
     login_frm.style.display = "none"
+    member_btn[1].classList.add("active")
     non_login_frm.style.display = "block"
 })
