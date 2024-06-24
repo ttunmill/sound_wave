7509f2ce3d17a327ec08021ba5ab35932a7177ba
TtunMill <84768554+ttunmill@users.noreply.github.com>
2024-06-24 18:11:02 +0900

    update

---
+++ mobile/pages/js/login.js
@@ -0,0 +1,37 @@
+const member_btn = document.querySelectorAll(".select_login a")
+const login_frm = document.querySelector("#user_login_form")
+const non_login_frm = document.querySelector("#non_user_form")
+
+non_login_frm.style.display = "none"
+
+member_btn[0].addEventListener("click", () => {
+    member_btn[1].classList.remove("active")
+    non_login_frm.style.display = "none"
+    member_btn[0].classList.add("active")
+    login_frm.style.display = "block"
+})
+
+member_btn[1].addEventListener("click", () => {
+    member_btn[0].classList.remove("active")
+    login_frm.style.display = "none"
+    member_btn[1].classList.add("active")
+    non_login_frm.style.display = "block"
+})
+// ============ 로그인 상태 유지 버튼
+const check_box = document.querySelector("#user_login_form .user_insert span")
+const check_img = document.querySelector("#user_login_form .user_insert img")
+console.log(check_box)
+
+let img_scr = "./images/login/bg_checkbox_checked.png"
+let img_original = "./images/login/bg_checkbox.png"
+
+let chk_flag = false;
+check_box.addEventListener("click", () => {
+    // let chk_message = confirm("로그인 상태를 유지하시겠습니까? 타인이 개인정보를 도용할 수 있으니, 주의하시기 바랍니다.")
+    chk_flag = !chk_flag
+    if(chk_flag == true) {
+        check_img.src = img_scr
+    } else {
+        check_img.src = img_original
+    }
+})
